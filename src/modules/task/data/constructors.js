c2d2ebfbf6896c3f49e4a32878acb2220679db75
Rafael Kendrik <rafaelkendrik@gmail.com>
2020-04-02 10:47:26 -0300

    Improve task constructor
    - Created TaskStatus Class
    - Added basic JSDoc Documentation
    - Added status enums

--- src/modules/task/data/constructors.js
+++ src/modules/task/data/constructors.js
@@ -1,15 +1,67 @@
-export class Task {
-  id
-  title
-  status
-  description
-  timestamp
+/**
+ * Status Labels Enum
+ * @constant
+ * @type {Array}
+ */
+const STATUS_LABELS = {
+  0: 'Pending',
+  1: 'In Progress',
+  2: 'Done'
+}
 
-  constructor ({ id, title, status, description, timestamp }) {
+/**
+ * Status Names Enum - Status Labels Enum in kebab-case
+ * @constant
+ * @type {Array}
+ */
+const STATUS_NAMES = Object.entries(STATUS_LABELS)
+  .reduce((acc,[id, label]) => (
+    acc[id] = label
+      .toLowerCase()
+      .split(' ')
+      .join('-'),
+    acc
+  ), {})
+
+/**
+ * Task Status Class.
+ * @constructor
+ * @param {string} id - The ID of the task status.
+ */
+export class TaskStatus {
+  constructor (id) {
+    this.id = id
+    this.label = this._getLabel()
+    this.name = this._getName()
+  }
+
+  /** @private */
+  _getLabel () {
+    return STATUS_LABELS[this.id]
+  }
+
+  /** @private */
+  _getName () {
+    return STATUS_NAMES[this.id]
+  }
+}
+
+
+/**
+ * Task Class.
+ * @constructor
+ * @param {string} id - The ID of the task.
+ * @param {string} title - The title of the task.
+ * @param {string} description - The description of the task.
+ * @param {number} timestamp - The timestamp of the task.
+ * @param {number} statusId - The status id of the task.
+ */
+export class Task {
+  constructor ({ id, title, statusId, description, timestamp }) {
     this.id = id
     this.title = title
-    this.status = status
     this.description = description
     this.timestamp = timestamp
+    this.status = new TaskStatus(statusId)
   }
 }
--- src/modules/task/data/services.js
+++ src/modules/task/data/services.js
@@ -1,3 +1,5 @@
 export const fetchTasks = () => Promise.resolve([
-  { title: 'task title', status: 1, description: 'this is a description', timestamp: null }
+    { title: 'task title', statusId: 0, description: 'this is the description of a pending task', timestamp: 987654321 },
+    { title: 'task title', statusId: 1, description: 'this is the description of a in-progress task', timestamp: 987654321 },
+    { title: 'task title', statusId: 2, description: 'this is the description of a already done task', timestamp: 987654321 }
 ])
