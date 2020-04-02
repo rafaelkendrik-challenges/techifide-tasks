826164c9e834eb625a96c03f1462d8120a2cc99b
Rafael Kendrik <rafaelkendrik@gmail.com>
2020-04-02 10:38:38 -0300

    Add SCSS configs
    - Added chainWebpack to vue.config.js
      (https://cli.vuejs.org/guide/css.html)
    - Added few dependencies
    - Added postcss.config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
 const path = require('path')
 
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        //path.resolve(__dirname, './src/styles/imports.styl'),
+      ],
+    })
+}
+
 module.exports = {
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+  },
+
   configureWebpack: {
     resolve: {
       alias: {
