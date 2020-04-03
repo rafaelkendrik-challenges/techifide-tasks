6a3ee5ccb5dd84f2a4be31feddc93d334ad51100
Rafael Kendrik <rafaelkendrik@gmail.com>
2020-04-02 22:16:05 -0300

    Create fakeAxios to separate concerns

--- src/modules/task/data/services.js
+++ src/modules/task/data/services.js
@@ -1,11 +1,31 @@
-export const fetchTasks = () => Promise.resolve([
-  { id: 'id001', title: 'task title', statusId: 0, description: 'this is the description of a pending task', timestamp: 987654321 },
-  { id: 'id002', title: 'task title', statusId: 1, description: 'this is the description of a in-progress task', timestamp: 987654321 },
-  { id: 'id003', title: 'task title', statusId: 2, description: 'this is the description of a already done task', timestamp: 987654321 }
-])
+export const fetchTasks = () =>
+  fakeAxios('/tasks')
 
-export const storeTask = ({ title, description }) => Promise.resolve({
-  id: 'id004', title, description, statusId: 0, timestamp: 987654321
-})
+export const storeTask = params =>
+  fakeAxios('/task', params)
 
-export const storeTaskStatus = () => Promise.resolve()
+export const storeTaskStatus = params =>
+  fakeAxios('/task/status', params)
+
+/** @description Conside this part only for mocking propose, in a real world application, this function is replaced by an HTTP client, axios for e.g. */
+const fakeAxios = (url, params) => {
+  if (url === '/tasks') {
+    return Promise.resolve([
+      { id: 'id001', title: 'task title', statusId: 0, description: 'this is the description of a pending task', timestamp: 1585875534055 },
+      { id: 'id002', title: 'task title', statusId: 1, description: 'this is the description of a in-progress task', timestamp: 1585875534055 },
+      { id: 'id003', title: 'task title', statusId: 2, description: 'this is the description of a already done task', timestamp: 1585875534055 }
+    ])
+  }
+
+  if (url === '/task') {
+    const { description, title } = params
+
+    return Promise.resolve({
+      id: 'id004', title, description, statusId: 0, timestamp: (new Date()).getTime()
+    })
+  }
+
+  if (url === '/task/status') {
+    return Promise.resolve()
+  }
+}
